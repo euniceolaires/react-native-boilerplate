1d01447d70e1bca934fa295fe6592e5d46b79f1d
Eunice Olaires <euniceolaires@Eunices-MacBook-Pro.local>
2018-07-24 18:59:58 +0800

    [ENHANCEMENT]: Extracted navigation config/routes folder

--- app/index.js
+++ app/index.js
@@ -1,48 +1,26 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-import { Navigation } from 'react-native-navigation';
 import { Provider } from 'react-redux';
 
 import * as navigation from './actions/navigation';
 import registerScreens from './config/screens';
 import store from './config/store';
+import navigator from './config/routes';
 
 registerScreens(store, Provider);
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.startApp();
-    // store.subscribe(this.onStoreUpdate.bind(this));
-    // store.dispatch(navigation.changeAppRoot('login'));
+    store.subscribe(this.onStoreUpdate.bind(this));
+    store.dispatch(navigation.changeAppRoot('home'));
   }
 
-  // onStoreUpdate() {
-  //   const { root } = store.getState().navigation;
-  //   if (this.currentRoot !== root) {
-  //     this.currentRoot = root;
-  //     this.startApp(root);
-  //   }
-  // }
-
-  startApp(root) {
-    Navigation.startTabBasedApp({
-      tabs: [
-        {
-          label: 'One',
-          screen: 'Screen1',
-          icon: require('./images/icon1.png'),
-          selectedIcon: require('./images/icon1_selected.png'),
-          title: 'Screen One',
-        },
-        {
-          label: 'Two',
-          screen: 'Screen2',
-          icon: require('./images/icon2.png'),
-          selectedIcon: require('./images/icon2_selected.png'),
-          title: 'Screen Two',
-        },
-      ],
-    });
+  onStoreUpdate() {
+    const { root } = store.getState().navigation;
+    if (this.currentRoot !== root) {
+      this.currentRoot = root;
+      navigator(root);
+    }
   }
 }

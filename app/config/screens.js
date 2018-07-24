a51f4915646ac3a757dd016d4b5905436f37cc23
Eunice Olaires <euniceolaires@Eunices-MacBook-Pro.local>
2018-07-24 17:06:38 +0800

    [ENHANCEMENT]: Moved navigation to app.js

---
+++ app/config/screens.js
@@ -0,0 +1,13 @@
+import { Navigation } from 'react-native-navigation';
+
+import Screen1 from '../screens/Screen1';
+import Screen2 from '../screens/Screen2';
+import Screen3 from '../screens/Screen3';
+import Screen4 from '../screens/Screen4';
+
+export default (store, Provider) => {
+  Navigation.registerComponent('Screen1', () => Screen1);
+  Navigation.registerComponent('Screen2', () => Screen2);
+  Navigation.registerComponent('Screen3', () => Screen3);
+  Navigation.registerComponent('Screen4', () => Screen4);
+};
--- app/index.js
+++ app/index.js
@@ -1,6 +1,35 @@
 // import registerScreens from './screens/index';
-import routes from './config/routes';
+// import routes from './config/routes';
+// import React from 'react';
+
+// import Navigator from './config/routes';
+
+// export default () => (
+//   <Navigator />
+// );
+import { Navigation } from 'react-native-navigation';
+
+import registerScreens from './config/screens';
 
 export default () => {
-  routes();
-};
+  registerScreens();
+
+  Navigation.startTabBasedApp({
+    tabs: [
+      {
+        label: 'One',
+        screen: 'Screen1',
+        icon: require('./images/icon1.png'),
+        selectedIcon: require('./images/icon1_selected.png'),
+        title: 'Screen One',
+      },
+      {
+        label: 'Two',
+        screen: 'Screen2',
+        icon: require('./images/icon2.png'),
+        selectedIcon: require('./images/icon2_selected.png'),
+        title: 'Screen Two',
+      },
+    ],
+  });
+}
